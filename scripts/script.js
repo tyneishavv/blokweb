3be67eb43f390c7ec59c996516149fd46a898a6f
Tyneisha <124717910+tyneishavv@users.noreply.github.com>
2025-10-22 21:53:10 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+// HAMBURGER NAV ONTWIKKELEN
+
+var menuButton = document.querySelector("body header nav:nth-of-type(2) ul li button")
+var hamburgerNav = document.querySelector("body header nav:nth-of-type(3) ul")
+
+menuButton.addEventListener("click", function () {
+    console.log("menuButton geklikt!")
+    hamburgerNav.classList.toggle("yay")
+})
